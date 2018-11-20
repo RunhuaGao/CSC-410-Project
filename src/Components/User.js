41fc19aa32f639064021e5b8a55381324cf4fc13
Runhua Gao <43771629+RunhuaGao@users.noreply.github.com>
2018-11-20 14:02:08 -0500

    User info Storage Public class

---
+++ src/Components/User.js
@@ -0,0 +1,10 @@
+class User {
+    constructor(firstname,lastname,age,sex,contestgroup){
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.age = age;
+        this.sex = sex;
+        this.contestgroup = contestgroup;
+    }
+}
+export default User;
