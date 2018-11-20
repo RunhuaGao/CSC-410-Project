659e906192c4642ba8f9b8858cd107a2fe0121df
Runhua Gao <43771629+RunhuaGao@users.noreply.github.com>
2018-11-20 13:49:54 -0500

    Game public Class used for first page and game history
    Game public class

---
+++ src/Components/Game.js
@@ -0,0 +1,8 @@
+class Game{
+    constructor(player1,player2,courtnum,result){
+        this.player1 = player1;
+        this.player2 = player2;
+        this.cournum = courtnum;
+        this.result = result; 
+    }
+}
