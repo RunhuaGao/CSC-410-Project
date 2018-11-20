bdb1a30380d5d5f5b0694df72ec24cce35b48ed4
CalyxCorolla <45017976+CalyxCorolla@users.noreply.github.com>
2018-11-20 13:39:59 -0500

    NavBar component of homepage and SignUp component

---
+++ src/Components/NavBar.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem } from 'reactstrap';
+
+
+
+
+
+class NavBar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+    toggle() {
+        this.setState({
+          isOpen: !this.state.isOpen
+        });
+    }
+
+
+
+    render() {
+        return (
+            <div>
+              <Navbar color="light" light expand="md">
+                <NavbarBrand href="/">2018 Badminton Contest</NavbarBrand>
+                <NavbarToggler onClick={this.toggle} />
+                <Collapse isOpen={this.state.isOpen} navbar>
+                  <Nav className="ml-auto" navbar>
+                    <NavItem>
+                      <NavLink onClick>Log In</NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink onClick>Sign Up</NavLink>
+                    </NavItem>
+                    <UncontrolledDropdown nav inNavbar>
+                      <DropdownToggle nav caret>
+                        More Information
+                      </DropdownToggle>
+                      <DropdownMenu right>
+                        <DropdownItem>
+                          Rules
+                        </DropdownItem>
+                        <DropdownItem>
+                          Awards
+                        </DropdownItem>
+                        <DropdownItem divider />
+                        <DropdownItem>
+                          Reset
+                        </DropdownItem>
+                      </DropdownMenu>
+                    </UncontrolledDropdown>
+                  </Nav>
+                </Collapse>
+              </Navbar>
+            </div>
+            
+        );
+    };
+}      
+
+
+
+export default NavBar;
