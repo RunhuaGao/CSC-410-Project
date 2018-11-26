254840294b9f4606d2bf9b9424368696d490baa8
shiningstark <44914793+shiningstark@users.noreply.github.com>
2018-11-25 22:55:51 -0500

    Add files via upload

---
+++ src/Components/test.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import fetch from 'cross-fetch';
+
+class Test extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            name : "world",
+            message:"hello ",
+            servermessage:""
+        }
+        global.name = ""
+        this.handleChange = this.handleChange.bind(this) ;
+    }
+
+    handleChange(name){
+        console.log(this.refs.names.value)
+        fetch('http://localhost:3001/hello',{method:'GET'})
+            .then(
+                function(response){
+                    // console.log(response)
+                    return response.json()
+                }
+            )
+            .then(
+                data => {
+                    this.setState({
+                        name: this.refs.names.value,
+                        servermessage:data[0].message
+                    })
+                }
+            )
+            .catch(function(err){
+                console.log("Fetch错误:"+err);
+        });
+    }
+    renderMessage(message){
+        return(
+            <div>
+                {message}
+            </div>
+        )
+    }
+    render(){
+        return(
+            <div>
+                <div>{this.state.message} {this.state.name}</div>
+                <div>{this.state.message} {this.state.servermessage}</div>
+                <div>
+                    <label for="chat">Message</label>
+                    <input type="text" placeholder="input message" ref="names" maxLength="3"/>
+                    <button type="button" className="btn btn-info" value= {this.state.message} onClick={this.handleChange.bind(this)}>Submit</button>
+                </div>
+            </div>
+        );
+    }
+}
+export default Test;
