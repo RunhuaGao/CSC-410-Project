ba44b917ba67fec2a99a7bf93095bdee71a76e56
shiningstark <44914793+shiningstark@users.noreply.github.com>
2018-12-06 15:43:16 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
 import React, { Component } from 'react';
-import NavBar from './components/NavBar';
-import Live from './components/Live';
-import ImgHomepage from './components/ImgHomepage';
-import Timer from './components/Timer';
 import './App.css';
-
+import Home from './components/Home';
+import { BrowserRouter } from 'react-router-dom';
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
-      <div className="App">
-        <NavBar />    
-        <ImgHomepage />
-        <Live />
-        <Timer />
-      </div>
-
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
     );
   }
 }
-
-
-
 export default App;
