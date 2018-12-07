66b74efed678538a4d7a8cfc221c410c02ef8d15
Runhua Gao <43771629+RunhuaGao@users.noreply.github.com>
2018-12-06 21:35:40 -0500

    Could send game history to client

--- ServerSide/loginRouter.js
+++ ServerSide/loginRouter.js
@@ -2,7 +2,6 @@ const express = require("express");
 const game = require('../models/game');
 const player = require("../models/player");
 const bodyparser = require("body-parser");
-const gameRouter = express.Router();
 
 const loginRouter = express.Router();
 loginRouter.use(bodyparser.json());
@@ -17,15 +16,35 @@ loginRouter.route('/')
     player.find({email:e})
     .then(players=>{
         console.log(players);
-        if(players.length===0){
-            res.send("0");
-        } else if(players[0].password!=p){
-            res.send("0");
+        if(players.length===0 | players[0].password!=p){
+            res.send({"validation":false});
         } else{
-            res.send("1");
+            var playerinfo = players[0];
+            var gamekey = playerinfo.fullname;
+            console.log("name is "+gamekey);
+            var sendinfo = {"validation":1,"info":playerinfo,"game":null};
+            game.find().or([{player1:gamekey},{player2:gamekey}])
+            .then((games)=>{
+                console.log(games);
+                if(games.length===0){
+                    console.log("Donot find games");
+                    res.send(sendinfo);
+                }
+                else{
+                    sendinfo["game"] = games;
+                    res.send(sendinfo);
+                }
+            })
+            .catch(err=>{
+                console.log(err);
+                res.send(sendinfo);
+            });
         }
     })
-    .catch(err=>res.send("0"));
+    .catch(err=>{
+        console.log(err);
+        res.send("find error")
+    });
 });
 
 module.exports = loginRouter;
