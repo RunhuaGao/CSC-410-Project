c6a9e1f8d2554badadfed8a10272c36cdc81c870
shiningstark <44914793+shiningstark@users.noreply.github.com>
2018-12-11 18:16:33 -0500

    Add files via upload

---
+++ Front End Files/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import Home from './components/Home';
+import { BrowserRouter } from 'react-router-dom';
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
+    );
+  }
+}
+export default App;
